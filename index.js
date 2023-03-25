dc77ee1a8b86839417715c1b72f2b670e5c0b866
Laxi4u <85995291+Laxi4u@users.noreply.github.com>
2023-03-25 19:54:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Suresh Yerramsetti Jenkis" }');
+    res.send('{ "response": "Hello From Suresh CICD k8s" }');
 });
 
 app.get('/will', function (req, res) {
